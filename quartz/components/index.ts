cd11aa9f6ae567c5b7613660d2c0a478b24bb029
eldelapanaderia <130405535+eldelapanaderia@users.noreply.github.com>
2025-11-27 10:31:21 +0000

    Update index.ts

--- quartz/components/index.ts
+++ quartz/components/index.ts
@@ -23,7 +23,6 @@ import Breadcrumbs from "./Breadcrumbs"
 import Comments from "./Comments"
 import Flex from "./Flex"
 import ConditionalRender from "./ConditionalRender"
-import Infobox from "./Infobox"
 
 export {
   ArticleTitle,
@@ -51,5 +50,4 @@ export {
   Comments,
   Flex,
   ConditionalRender,
-  Infobox,
 }
