4089adc6fb50e97954b109af5abd5c5ee91247c4
eldelapanaderia <130405535+eldelapanaderia@users.noreply.github.com>
2025-11-27 11:09:06 +0000

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -1,5 +1,6 @@
 import { PageLayout, SharedLayout } from "./quartz/cfg"
 import * as Component from "./quartz/components"
+import InfoboxComponent from "./components/Infobox"
 
 // components shared across all pages
 export const sharedPageComponents: SharedLayout = {
@@ -40,6 +41,7 @@ export const defaultContentPageLayout: PageLayout = {
       ],
     }),
     Component.Explorer(),
+    InfoboxComponent(),
   ],
   right: [
     Component.Graph(),
