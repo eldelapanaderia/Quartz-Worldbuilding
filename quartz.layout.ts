fbcec48571388b6b18e20f731a1937d921eb0fae
eldelapanaderia <130405535+eldelapanaderia@users.noreply.github.com>
2025-11-27 15:02:53 +0000

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -4,7 +4,7 @@ import * as Component from "./quartz/components"
 // components shared across all pages
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
-  header: [],
+  header: [Component.NavLinks()],
   afterBody: [],
   footer: Component.Footer(),
 }
