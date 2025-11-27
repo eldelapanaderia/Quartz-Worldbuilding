0d2b12055e4ad6dcc562d7217c82c4f93e9bf970
eldelapanaderia <130405535+eldelapanaderia@users.noreply.github.com>
2025-11-27 10:35:44 +0000

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -6,6 +6,7 @@ export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
   afterBody: [],
+  footer: [],
   //footer: Component.Footer({
   //  links: {
   //    GitHub: "https://github.com/jackyzha0/quartz",
